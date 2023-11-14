86844ba0d54bde6e030131aa9d702cb941b33c97
Kirno <kirno@natyx.net>
2023-11-14 23:15:53 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,57 @@
+## Inventory Management System
+
+### Overview
+
+This is a simple command-line inventory management system written in Python. The program allows users to perform various actions on an inventory, such as giving items, withdrawing items, exporting and importing the inventory, debugging, and resetting the inventory.
+
+### Features
+
+- **Give Items:** Users can give items like swords, bows, and guns to the inventory.
+- **Withdraw Items:** Users can withdraw the last item added to the inventory.
+- **Export/Import:** The inventory can be exported to a save file and later imported to resume the inventory state.
+- **Username:**Users can have their username personalized to their image
+- **Debugging:** Provides debug information about the current state of the inventory.
+- **Reset:** Resets the inventory, clearing all items.
+
+### How to Use
+
+1. **Give Items (`G`):** Choose from the available items (Sword, Bow, Gun) to give to the inventory.
+
+2. **Withdraw Items (`W`):** Withdraw the last item added to the inventory.
+
+3. **Export Inventory (`E`):** Save the current state of the inventory to a file.
+
+4. **Import Inventory (`I`):** Load a previously saved inventory state from a file.
+
+5. **Set Name (`N`):** Create or update your name in the program.
+
+6. **Debug (`D`):** View debug information about the inventory, including the file paths and current items.
+
+7. **Reset (`R`):** Clear all items in the inventory and reset it.
+
+### Inventory Display
+
+The inventory display shows a graphical representation of the items in the inventory using symbols like ⚔️ (sword), 🏹 (bow), and 🔫 (gun).
+
+### Error Handling
+
+If an invalid action is entered, the program will display an error message and wait for 2 seconds before returning to the main menu.
+
+### File Paths
+
+- **Inventory File:** "./logs/inv.txt"
+- **Save File:** "./data/save.txt"
+
+### How to Run
+
+1. Make sure you have Python installed on your system.
+
+2. Run the script using the command:
+
+   ```
+   python index.py
+   ```
+
+### License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ index.py
@@ -0,0 +1,210 @@
+# MIT License
+# Copyright (c) 2023 Kirno
+
+# - - - - - - - - - - - - - - - - - - - # IMPORT # - - - - - - - - - - - - - - - - - - - #
+
+import os
+import time
+
+# - - - - - - - - - - - - - - - - - - # CLEAR MODULE # - - - - - - - - - - - - - - - - - - #
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# - - - - - - - - - - - - - - - - # HOME CHOICE MODULE # - - - - - - - - - - - - - - - - #
+
+def home_choice():
+    while True:
+        clear()
+        inventory.display_inventory()
+        inventory.display_menu()
+
+        choice = input(f"{inventory.user_name}> ").lower()
+
+        if choice == "g":
+            inventory.give()
+            print(f"\n")
+
+        elif choice == "w":
+            inventory.withdraw()
+            print(f"\n")
+
+        elif choice == "e":
+            inventory.export_save()
+            print(f"\n")
+
+        elif choice == "i":
+            inventory.import_save()
+            print(f"\n")
+
+        elif choice == "s":
+            inventory.set_name()
+            print(f"\n")
+
+        elif choice == "d":
+            inventory.debug()
+            print(f"\n")
+
+        elif choice == "r":
+            inventory.reset()
+            print(f"\n")
+
+        else:
+            clear()
+            print(f"[ERROR] Invalid action wait 2 secondes and retry please...")
+            time.sleep(2)
+
+# - - - - - - - - - - - - - - - - - - # INVENTORY CLASS # - - - - - - - - - - - - - - - - - - #
+
+class Inventory:
+    def __init__(self):
+        self.inventory_file = "./logs/inv.txt"
+        self.save_file = "./data/save.txt"
+        self.max_items = 7
+        self.items = []
+
+# - - - - - - - - - - - - - - - - - - # DEBUG INV MODULE # - - - - - - - - - - - - - - - - - - #
+
+    def debug(self):
+        with open(self.save_file, "r", encoding="utf-8") as file:
+            temp_save = [line.strip() for line in file.readlines()]
+
+        clear()
+        print(f"=== Inventory Debug Information ===")
+        print(f"Inventory File: {self.inventory_file}")
+        print(f"Save File: {self.save_file}")
+        print(f"Items Save : {temp_save}")
+        print(f"Items: {self.items}")
+        print(f"===================================\n")
+        print(f"Press a key to exit\n\n")
+        input(f"{self.user_name}> ")
+
+# - - - - - - - - - - - - - - - - - - # DISPLAY INV MODULE # - - - - - - - - - - - - - - - - - - #
+
+    def display_inventory(self):
+        print(f"|◇───◇────────◇───────────◇  ◇───◇────────◇───────────◇|")
+        for item in self.items:
+            print(f"◇◇ {item} ◇◇", end="")
+        print(f"\n|◇───────────◇────────◇───◇  ◇───────────◇────────◇───◇|")
+
+# - - - - - - - - - - - - - - - - - # DISPLAY MENU INV MODULE # - - - - - - - - - - - - - - - - - #
+
+    def display_menu(self):
+        print(f"◇────◇────────◇────────────◇◇────◇────────◇────────────◇")
+        print(f"|                Welcome to your Inventory             |")
+        print(f"◇────────────◇────────◇────◇◇────────────◇────────◇────◇\n")
+        print(f"◇─◇─◇───────────◇   ◇─◇─◇───────────◇   ◇─◇─◇───────────◇")
+        print(f"| G : Give      |   | E : Export    |   | S : Set name  |")
+        print(f"◇───────────◇─◇─◇   ◇───────────◇─◇─◇   ◇───────────◇─◇─◇")
+        print(f"| W : Withdraw  |   | I : Import    |")
+        print(f"◇─◇─◇───────────◇   ◇─◇─◇───────────◇")
+        print(f"◇───────────◇─◇─◇   ◇───────────◇─◇─◇")
+        print(f"| D : Debug     |   | R : Reset     |")
+        print(f"◇─◇─◇───────────◇   ◇─◇─◇───────────◇\n")
+
+# - - - - - - - - - - - - - - - - - - # GIVE INV MODULE # - - - - - - - - - - - - - - - - - - #
+
+    def give(self):
+        clear()
+        inventory.display_inventory()
+        print(f"◇────◇────────◇────────────◇◇────◇────────◇────────────◇")
+        print(f"|             Select an item with its name             |")
+        print(f"◇────────────◇────────◇────◇◇────────────◇────────◇────◇\n")
+        print(f"◇────◇────────◇────────────◇◇────◇────────◇────────────◇")
+        print(f"|        ⚔️ ⌂ Sword      🏹 ⌂ Bow      🔫 ⌂ Gun        |")
+        print(f"◇────────────◇────────◇────◇◇────────────◇────────◇────◇\n")
+        item_give = input(f"{self.user_name}> ")
+        if len(self.items) < self.max_items:
+            if item_give in ["sword", "bow", "gun"]:
+                with open(self.inventory_file, "a", encoding="utf-8") as file:
+                    file.write(f"[{item_give.upper()}] has been given\n")
+                self.items.append(self.item_skin(item_give))
+            else:
+                print(f"[ERROR] Invalid item name back to home in 2 seconds...")
+                time.sleep(2)
+        else:
+            print(f"[ERROR] Maximum number of items reached back to home in 2 seconds...")
+            time.sleep(2)
+
+# - - - - - - - - - - - - - - - - - - # WITHDRAW INV MODULE # - - - - - - - - - - - - - - - - - - #
+
+    def withdraw(self):
+        if self.items:
+            withdrawn_item = self.items.pop()
+            with open(self.inventory_file, "a", encoding="utf-8") as file:
+                file.write(f"[{withdrawn_item.upper()}] has been withdrawn\n")
+        else:
+            print(f"[ERROR] Inventory is empty. Nothing to withdraw wait 2 seconds...")
+            time.sleep(2)
+
+# - - - - - - - - - - - - - - - - - - # EXPORT INV MODULE # - - - - - - - - - - - - - - - - - - #
+
+    def export_save(self):
+        with open(self.save_file, "w", encoding="utf-8") as file:
+            file.write(f"{self.user_name};")
+            file.write(";".join(self.items))
+            
+        with open(self.inventory_file, "a", encoding="utf-8") as file:
+            file.write(f"[EXPORT] Inventory saved successfully\n")
+        
+        clear()
+        inventory.display_inventory()
+        print(f"◇────◇────────◇────────────◇◇────◇────────◇────────────◇")
+        print(f"|            You can now import your backup            |")
+        print(f"|              directly from the home menu             |")
+        print(f"◇────────────◇────────◇────◇◇────────────◇────────◇────◇\n")
+        print(f"Press a key to exit\n\n")
+        input(f"{self.user_name}> ")
+
+# - - - - - - - - - - - - - - - - - - # IMPORT INV MODULE # - - - - - - - - - - - - - - - - - - #
+
+    def import_save(self):
+        with open(self.save_file, "r", encoding="utf-8") as file:
+            content = file.read().split(";")
+            self.user_name = content[0].strip()
+            self.items = content[1:]
+        
+        with open(self.inventory_file, "a", encoding="utf-8") as file:
+            file.write(f"[IMPORT] Inventory loaded successfully\n")
+
+
+# - - - - - - - - - - - - - - - - - - # SET NAME INV MODULE # - - - - - - - - - - - - - - - - - - #
+
+    def set_name(self):
+        clear()
+        inventory.display_inventory()
+        print(f"◇────◇────────◇────────────◇◇────◇────────◇────────────◇")
+        print(f"|                  Enter your new name                 |")
+        print(f"◇────────────◇────────◇────◇◇────────────◇────────◇────◇\n")
+
+        past_name = self.user_name
+        self.user_name = input(f"{self.user_name}> ")
+        with open(self.inventory_file, "a", encoding="utf-8") as file:
+            file.write(f"[USERNAME] Successfully edit username from {past_name} to {self.user_name}\n")
+
+# - - - - - - - - - - - - - - - - - - # RESET INV MODULE # - - - - - - - - - - - - - - - - - - #
+
+    def reset(self):
+        self.items = []
+        self.user_name = ""
+        open(self.save_file, "w").close()
+        open(self.inventory_file, "w").close()
+
+# - - - - - - - - - - - - - - - - - - # SKIN INV MODULE # - - - - - - - - - - - - - - - - - - #
+
+    @staticmethod
+    def item_skin(item_id):
+        return "⚔️" if item_id == "sword" else ("🏹" if item_id == "bow" else "🔫")
+
+# - - - - - - - - - - - - - - - - - - # CODE LAUNCHER # - - - - - - - - - - - - - - - - - - #
+
+if __name__ == "__main__":
+    try:
+        inventory = Inventory()
+        inventory.user_name = ""
+        while True:
+            clear()
+            home_choice()
+
+    except Exception as error:
+        print(f"[ERROR] {error}")
